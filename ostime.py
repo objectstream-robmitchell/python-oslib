fdbee6c9fc13276f01454ed8670655cd1ab1c3c8
Rob Mitchell <rob.mitchell@objectstream.com>
2020-04-21 14:41:40 -0500

    ostime.py

--- ostime.py
+++ ostime.py
@@ -1,21 +1,41 @@
 
+
+"""
 __module__      = 'ostime.py'
 __author__      = 'Rob Mitchell <rob.mitchell@objectstream.com>'
-__last_update__ = '2020.03.04'
+__updated__     = '2020.03.04'
 __version__     = '0.0.1'
 __status__      = 'development'
 __description__ = 'Objectstream Time Utilities' 
+"""
+
+# ToDo:
+# - needs class and method docs
 
 
 import time
 import datetime
 
 class OSTime:
+	"""
+	OSTime class is used to get formatted time values.
+	"""
+
 	def epoch(self,string=False):
 		if string:
 			return str(time.time()).split('.')[0]
 		else:
 			return int(time.time())
+
+	def hms(self):
+		n = datetime.datetime.now()
+		return ''.join( [x if len(x) > 1 else '0'+x for x in (str(n.year),str(n.month),
+					str(n.day),str(n.hour),str(n.minute),str(n.second))] )
+
+	def ymd(self):
+		n = datetime.datetime.now()
+		return ''.join( [x if len(x) > 1 else '0'+x for x in (str(n.year),str(n.month),str(n.day))] )
+
 
 
 
