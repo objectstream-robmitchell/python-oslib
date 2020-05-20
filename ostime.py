4d4cc377dee65d26d2d4a505eea8ed7590ea540d
Rob Mitchell <rob.mitchell@objectstream.com>
2020-05-20 16:38:34 -0500

    ostime.py - added utc

--- ostime.py
+++ ostime.py
@@ -2,42 +2,44 @@
 """
 __module__      = 'ostime.py'
 __author__      = 'Rob Mitchell <rob.mitchell@objectstream.com>, <rlmitchell@gmail.com>'
-__updated__     = '2020.04.27'
-__version__     = '1.0.0'
+__updated__     = '2020.05.20'
+__version__     = '1.1.0'
 __status__      = 'development'
 __description__ = 'Objectstream Time Utilities' 
 
 __history__     = '''
+						2020.05.20 - added utc 
 						2020.03.04 - initial development
 						'''
 """
 
-# ToDo:
-# - needs class and method docs
-
 
 import time
 import datetime
+
 
 class OSTime:
 	"""
 	OSTime class is used to get formatted time values.
 	"""
 
-	def epoch(self,string=False):
-		"""Return current epoch.
+	EPOCH = datetime.datetime(1970, 1, 1, 0, 0)
 
-		:param string: return type str
-		:type  string: boolean 
 
-		:returns: current epoch
-		:rtype:   str or int
+	def __init__(self,utc=False):
+		self.now = datetime.datetime.now()
+		if utc:
+			self.now = self.now.utcnow()
+
+
+	def epoch(self):
+		"""Return epoch.
+
+		:returns: epoch
+		:rtype:   int
 		"""
-		
-		if string:
-			return str(time.time()).split('.')[0]
-		else:
-			return int(time.time())
+
+		return int((self.now - OSTime.EPOCH).total_seconds())
 
 
 	def hms(self):
@@ -45,7 +47,7 @@ class OSTime:
 
 		"""
 
-		n = datetime.datetime.now()
+		n = self.now
 		return ''.join( [x if len(x) > 1 else '0'+x for x in (str(n.year),str(n.month),
 					str(n.day),str(n.hour),str(n.minute),str(n.second))] )
 
@@ -55,19 +57,7 @@ class OSTime:
 
 		"""
 
-		n = datetime.datetime.now()
+		n = self.now
 		return ''.join( [x if len(x) > 1 else '0'+x for x in (str(n.year),str(n.month),str(n.day))] )
 
-
-
-
-if __name__ == '__main__':
-	ostime = OSTime()
-	print 'epoch():'
-	print ostime.epoch(), str(type(ostime.epoch())), '()'
-	print ostime.epoch(string=False), str(type( ostime.epoch(string=False) )), ''
-	print ostime.epoch(string=True), str(type( ostime.epoch(string=True) )), ''
-	print ostime.epoch(False), str(type( ostime.epoch(False) )), ''
-	print ostime.epoch(True), str(type( ostime.epoch(True) )), ''
-		
 
