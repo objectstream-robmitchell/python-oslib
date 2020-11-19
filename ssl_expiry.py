844523a14a2fe31d6f039ad52dc7a1b00278e802
Rob Mitchell <rob.mitchell@objectstream.com>
2020-11-19 16:32:09 +0000

    ssl_expiry.py

--- ssl_expiry.py
+++ ssl_expiry.py
@@ -18,22 +18,28 @@ class SSLExpiry:
 		Initialize SSLExpiry with site and port.
 	
 		Parameters:
-		site (string):  Site to inspect. ex: 'www.objectstream.com'
-		port (int):     Port to connect to.  Default: 443
+		- site (string):  Site to inspect.     Ex: 'www.objectstream.com'
+		- port (int):     Port to connect to.  Default: 443
 		"""
 
 		self.site = site
 		self.port = port
 
 
-	def expired(self,warning_days=5):
+	def expired(self,warning_days=10):
 		"""
+		Determine if SSL certificate is expired.
+
+		Parameters:
+		- warning_days (int):  Number of days to subtract from actual expiry date.  Default: 10
+
+		Returns:
+		- (bool):  True if within warning window.
 		"""
 
 		cert = ssl.get_server_certificate((self.site, self.port))
 		x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
 		expire_datetime = datetime.strptime(str(x509.get_notAfter())[2:10], '%Y%m%d')
 		return datetime.now() < expire_datetime - timedelta(days=warning_days) 
-
 
 
