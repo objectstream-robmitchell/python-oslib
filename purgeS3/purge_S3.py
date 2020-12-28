d1fc5725504c845cee0f89a7e84fe718c548f678
Rob Mitchell <rob.mitchell@objectstream.com>
2020-12-28 16:39:00 +0000

    purge_S3.py refactor

--- purgeS3/purge_S3.py
+++ purgeS3/purge_S3.py
@@ -1,6 +1,6 @@
 
 __module__      = 'purge_s3.py'
-__author__      = 'Rob Mitchell <rob.mitchell@objectstream.com>'
+__contact__     = 'Rob Mitchell <rob.mitchell@objectstream.com>'
 __updated__     = '2020.06.17'
 __version__     = '0.5.0'
 __status__      = 'development, final'
@@ -17,19 +17,19 @@ class S3BucketPurger(object):
 	def __call__(self):
 		self._purge()
 
-	def _purge(self):
-		for backup_object_group in self.config['backup_object_groups']:
-			backup_object_group_keys = self._get_backup_object_keys_in_group(backup_object_group)
-			while len(backup_object_group_keys) > backup_object_group['keep_num']:
-				self._remove_object( backup_object_group_keys.pop(0) )
+#	def _purge(self):
+#		for backup_object_group in self.config['backup_object_groups']:
+#			backup_object_group_keys = self._get_backup_object_keys_in_group(backup_object_group)
+#			while len(backup_object_group_keys) > backup_object_group['keep_num']:
+#				self._remove_object( backup_object_group_keys.pop(0) )
 
-	def _get_backup_object_keys_in_group(self,backup_object_group):
-		backup_group_object_keys = []
-		for backup_object in self.all_objects:
-			if backup_object_group['key_component'] in backup_object.key:
-				backup_group_object_keys.append(backup_object.key)
-		backup_group_object_keys.sort()
-		return backup_group_object_keys
+#	def _get_backup_object_keys_in_group(self,backup_object_group):
+#		backup_group_object_keys = []
+#		for backup_object in self.all_objects:
+#			if backup_object_group['key_component'] in backup_object.key:
+#				backup_group_object_keys.append(backup_object.key)
+#		backup_group_object_keys.sort()
+#		return backup_group_object_keys
 
 	def _remove_object(self, object_key):
 		for backup_object in self.all_objects:
