1e0f2b8d93ead52ef627bdca51744fd25b52c7ad
Rob Mitchell <rob.mitchell@objectstream.com>
2020-12-29 22:31:27 +0000

    oslib_all_imports.py

--- util/tmp_storage.py
+++ util/tmp_storage.py
@@ -1,15 +1,23 @@
 import os
+import subprocess
 
-class SSHFS2():
+
+class TmpStorage():
 
 	def __init__(self):
 		self._check_user_id()
 		self.config = {'remote':'root@sslhelper.okc.objectstream.com:/mnt/backup_workspace','local':'/mnt/backup_workspace'}
+
+	def is_mounted(self):
+		for line in subprocess.getoutput('/usr/bin/mount').split('\n'):
+			if self.config['remote'] in line:
+				return True
+		return False
 
 	def _check_user_id(self):
 		if not os.getuid() == 0:
 			raise Exception('SSHFS must be envoked by the roor user.')
 
 
-SSHFS2()
+
 
