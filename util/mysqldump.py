3f7431c36bc385bc3f5ff9c249296221202d3b1b
Rob Mitchell <rlmitchell@gmail.com>
2021-01-20 15:49:55 -0600

    util/mysqldump.py

--- util/mysqldump.py
+++ util/mysqldump.py
@@ -1,2 +1,26 @@
-i
+
+__module__     = 'mysqldump.py'
+__maintainer__ = 'Rob Mitchell <rob.mitchell@objectstream.com>,<rlmitchell@gmail.com>'
+__tested__     = 'Python 3.6.8'
+__version__    = '2021.01.20.'
+
+
+import subprocess
+
+class MySQLDump:
+	def __init__(self,
+
+
+	def run_checks(self):
+		pass
+
+	def check_aes_key(self):
+		pass
+
+	def check_gpg(self):
+		pass
+
+	def check_mysqldump(self):
+		pass
+
 
