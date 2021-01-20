d7d99f067d73b91e247ff4ceee63d2e7e4c3747e
Rob Mitchell <rlmitchell@gmail.com>
2021-01-20 12:56:08 -0600

    purgeS3.py

--- util/purgeS3.py
+++ util/purgeS3.py
@@ -2,7 +2,7 @@
 __module__     = 'purgeS3.py'
 __maintainer__ = 'Rob Mitchell <rob.mitchell@objectstream.com>,<rlmitchell@gmail.com>'
 __tested__     = 'Python 3.5.2'
-__version__    = '2021.01.20.1217'
+__version__    = '2021.01.20.1254'
 
 
 import boto3
@@ -19,15 +19,5 @@ class PurgeS3:
 		keys = sorted([o.key for o in self.objects])
 		while len(self.objects) > self.keep:
 			delete_object = self.objects.pop(0)
-			print( delete_object )
 			response = delete_object.delete()
-			print( response )
 
-
-if __name__ == '__main__':
-	p = PurgeS3(backup_prefix='autobck.workstation.nginx.',num_keep=8,bucket_name='tmp1.objectstream.com')()
-
-
-'''
-['autobck.workstation.nginx.1609435215', 'autobck.workstation.nginx.1609435217', 'autobck.workstation.nginx.1609435219', 'autobck.workstation.nginx.1609435221', 'autobck.workstation.nginx.1609435223', 'autobck.workstation.nginx.1609435225', 'autobck.workstation.nginx.1609435227', 'autobck.workstation.nginx.1609435229', 'autobck.workstation.nginx.1609435231', 'autobck.workstation.nginx.1609435233']
-'''
