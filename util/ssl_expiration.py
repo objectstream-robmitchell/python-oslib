92a6f0fea63891330590c0d238fa5a9c140c3b14
Rob Mitchell <rlmitchell@gmail.com>
2021-02-10 16:08:10 -0600

    ssl_expiration.py

--- util/ssl_expiration.py
+++ util/ssl_expiration.py
@@ -3,8 +3,14 @@ import socket
 import datetime
 
 
+__module__     = 'ssl_expiration.py'
+__maintainer__ = 'Rob Mitchell <rob.mitchell@objectstream.com>,<rlmitchell@gmail.com>'
+__tested__     = 'Python 3.5.2'
+__version__    = '2021.02.10.1605'
+
+
 class SSLExpiration:
-    def __init__(self, host):
+    def __init__(self, host: str):
         self.host = host
         self.expire_dt = self._get_expiration()
         self.expire_date = str(self.expire_dt).split()[0]
@@ -16,6 +22,9 @@ class SSLExpiration:
         con.connect((self.host, 443))
         info = con.getpeercert()
         return datetime.datetime.strptime(info['notAfter'], '%b %d %H:%M:%S %Y %Z')
+
+    def expires(self):
+        return self.expire_date
 
     def __str__(self):
         return f"{self.host} SSL certificate expires on {self.expire_date}"
